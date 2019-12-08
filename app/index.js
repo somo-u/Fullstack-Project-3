9eaa083f134ea73c0e3f37bf3bb3c94f328d2f2a
Athoug Alsoughayer <athoug2.s@gmail.com>
2019-12-08 14:42:54 +0300

    project 3 base code

---
+++ app/index.js
@@ -0,0 +1,11 @@
+//  استيراد المكتبات المطلوبة | import the required libraries
+//  تأكد من تنزيل الوحدات المطلوبة | make sure to download the required modules
+
+
+
+
+
+
+
+
+// لا تنسى تحديد وظيفة الخادم | don't forget to define the server function that listens to requests
---
+++ app/routes/route.js
@@ -0,0 +1,14 @@
+// في هذا الملف ، قم بإعداد طرق التطبيق الخاصة بك | in this file, set up your application routes
+
+// 1. استيراد وحدة المستخدم | import the user module
+
+// 2. إعداد طرق مختلفة | setup the different routes (get, post, put, delete)
+
+
+
+
+
+
+
+
+// 3. تصدير الوحدة | export the module
