0f42e4cc53eeea3971fbd7e2be1e899c66c17db6
AymanAlqoqa <aalqouqa@gmail.com>
2019-12-29 11:32:43 +0200

    MOD: Add teacher auth.

---
+++ app/models/Student.js
@@ -0,0 +1,15 @@
+// في هذا الملف ، قم بإعداد وحدة المستخدم (الطالب) الخاصة بك | in this file, set up your user module
+
+// 1. قم باستيراد مكتبة moongoose | import the mongoose library
+
+// 2. قم بتحديد مخطط الطالب | start defining your user schema
+
+
+
+
+
+// 3. إنشاء نموذج الطالب | create  the user model
+
+
+
+// 4. تصدير الوحدة | export the module
